86d2092484dae56b4604744b3254e7be72bab288
Noahhuang78 <huangziyi897@gmail.com>
2024-04-11 10:46:37 +0800

    stylistic changes + loading screen

--- app/src/main/java/com/example/testapp/LoginPage.java
+++ app/src/main/java/com/example/testapp/LoginPage.java
@@ -1,6 +1,7 @@
 package com.example.testapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -24,6 +25,7 @@ public class LoginPage extends AppCompatActivity {
     MaterialButton loginbtn;
     MaterialButton signupbtn;
     ProgressBar progressBar;
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
