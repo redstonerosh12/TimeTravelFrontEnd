4ee2d4bb5a570576c4ac5d77a9ac457f6bcc1292
Benny <bennyrobot1143@gmail.com>
2024-04-14 14:08:47 +0800

    add config manager singleton

---
+++ app/src/main/java/com/example/testapp/lib/ConfigurationManager.java
@@ -0,0 +1,19 @@
+package com.example.testapp.lib;
+
+public class ConfigurationManager {
+    private static ConfigurationManager instance = null;
+    private String id;
+
+    public static ConfigurationManager getInstance() {
+        if (instance == null) instance = new ConfigurationManager();
+        return instance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
