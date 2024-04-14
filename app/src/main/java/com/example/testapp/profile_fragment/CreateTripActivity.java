a073f0bcfd5f3a598784e5092d540b11616a4720
Benny <bennyrobot1143@gmail.com>
2024-04-14 14:46:35 +0800

    update CreateTripActivity to use extended AppCompatActivity

--- app/src/main/java/com/example/testapp/profile_fragment/CreateTripActivity.java
+++ app/src/main/java/com/example/testapp/profile_fragment/CreateTripActivity.java
@@ -3,31 +3,28 @@ package com.example.testapp.profile_fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.EditText;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import com.example.testapp.MainActivity;
 import com.example.testapp.R;
+import com.example.testapp.lib.AppCompatActivity;
 import com.example.testapp.model.lib.DateTime;
 import com.example.testapp.model.TravelPlan;
-import com.example.testapp.model.lib.Toast;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CreateTripActivity extends AppCompatActivity {
-    private final String TAG = "CreateTripActivity";
-    private Toast toast;
+    public CreateTripActivity() {
+        super(R.layout.create_trip_form);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        toast = new Toast(CreateTripActivity.this);
-        setContentView(R.layout.create_trip_form);
 
         EditText editTitle = findViewById(R.id.editTitle);
         EditText editStartDate = findViewById(R.id.editStartDate);
@@ -36,42 +33,38 @@ public class CreateTripActivity extends AppCompatActivity {
 
         AppCompatButton submitForm = findViewById(R.id.SubmitFormCreateTrip);
 
-        submitForm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!isAllFieldsFilled(allFields)) {
-                    toast.makeShort("All fields must be filled in");
-                } else if (!isNumericallyFilled(editStartDate)) {
-                    toast.makeShort("Start date must be numerical!");
-                } else if (!isNumericallyFilled(editEndDate)) {
-                    toast.makeShort("End date must be numerical!");
-                } else {
-                    String tripHeader = extractText(editTitle);
-                    String startDate = startDateForTripModel(editStartDate);
-                    String endDate = startDateForTripModel(editEndDate);
-                    TripModel newTrip = new TripModel(tripHeader, startDate, endDate);
+        submitForm.setOnClickListener(view -> {
+            if (!isAllFieldsFilled(allFields)) {
+                toast.makeShort("All fields must be filled in");
+            } else if (!isNumericallyFilled(editStartDate)) {
+                toast.makeShort("Start date must be numerical!");
+            } else if (!isNumericallyFilled(editEndDate)) {
+                toast.makeShort("End date must be numerical!");
+            } else {
+                String tripHeader = extractText(editTitle);
+                String startDate = startDateForTripModel(editStartDate);
+                String endDate = startDateForTripModel(editEndDate);
 
-                    //FIXME: To add from frontend
-                    String startDay = "12";
-                    String startMonth = "04";
-                    String startYear = "2024";
+                //FIXME: To add from frontend
+                String startDay = "12";
+                String startMonth = "04";
+                String startYear = "2024";
 
-                    String endDay = "13";
-                    String endMonth = "04";
-                    String endYear = "2024";
+                String endDay = "13";
+                String endMonth = "04";
+                String endYear = "2024";
 
-                    LocalDate start = DateTime.stringToDate(startYear, startMonth, startDay);
-                    LocalDate end = DateTime.stringToDate(endYear, endMonth, endDay);
+                LocalDate start = DateTime.stringToDate(startYear, startMonth, startDay);
+                LocalDate end = DateTime.stringToDate(endYear, endMonth, endDay);
 
-                    TravelPlan.create(tripHeader, start, end).setOnResponse(travelPlan -> {
-                        Log.d(TAG, travelPlan.toString());
-                        toast.makeLong("Trip Created");
-                        Intent returnIntent = new Intent(CreateTripActivity.this, MainActivity.class);
-                        startActivity(returnIntent);
-                    }).setOnFailure(res -> {
-                        toast.makeLong("Failed to create trip");
-                    }).fetch();
-                }
+                TravelPlan.create(tripHeader, start, end).setOnResponse(travelPlan -> {
+                    Log.d(TAG, travelPlan.toString());
+                    toast.makeLong("Trip Created");
+                    Intent returnIntent = new Intent(CreateTripActivity.this, MainActivity.class);
+                    startActivity(returnIntent);
+                }).setOnFailure(res -> {
+                    toast.makeLong("Failed to create trip");
+                }).fetch();
             }
         });
     }
