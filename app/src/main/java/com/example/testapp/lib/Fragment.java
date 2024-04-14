4a8e7c786fc72eda324554a6e54ff9e9cac21bff
Benny <bennyrobot1143@gmail.com>
2024-04-14 13:10:14 +0800

    add Fragment extended

---
+++ app/src/main/java/com/example/testapp/lib/Fragment.java
@@ -0,0 +1,25 @@
+package com.example.testapp.lib;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.testapp.model.lib.Toast;
+
+public class Fragment extends androidx.fragment.app.Fragment {
+    protected final String TAG;
+    protected Toast toast;
+
+    protected Fragment() {
+        TAG = this.getClass().getSimpleName();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        toast = new com.example.testapp.model.lib.Toast(getActivity());
+    }
+
+    protected void goTo(Class<?> to) {
+        startActivity(new Intent(requireContext(), to));
+    }
+}
